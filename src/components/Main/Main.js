f93cf019e8f006ecb7d1cb5539f6ff20b163eca7
logickoff123 <SonyaUstinova@gmail.com>
2024-11-18 10:24:00 +0300

    header+allmain

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './Main.module.css';
-import oval1 from '../../img/oval1.svg';
-
+import all from '../../img/all.svg'
 const Main = () => {
     return (
         <div>
             <div className={styles.baner}>
-              <div classnamr = {styles.first_block}> 
-                  <h1 className={styles.white}>Прокачай <span className={styles.green}>знания</span></h1>
-                  <h1 className={styles.white}>для <span className={styles.green}>Успешной</span></h1>
-                  <h1 className={styles.white}>сдачи <span className={styles.green}>Экзаменов</span></h1>
-                  <img src = {oval1} alt = 'error' className = {styles.oval1}/>
+              <img src = {all} alt='error'/>
+              <div className={styles.textbox}>
+                <h1 className={styles.white}>Прокачай <span className={styles.green}>знания</span></h1>
+                <h1 className={styles.white}>для <span className={styles.green}>Успешной</span></h1>
+                <h1 className={styles.white}>сдачи <span className={styles.green}>Экзаменов</span></h1>
+                <p className ={styles.grey}>Предоставляем вам новейшую систему составления</p>
+                <p className ={styles.grey}>тестовых заданий на базе ИИ, которая поможет расширить</p>
+                <p className ={styles.grey}> ваши знания</p>
+                <button className = {styles.begin}>Начать</button>
               </div>
             </div>
         </div>
