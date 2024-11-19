7d6085dedad1ea010794fc5d32e1ac84ec23b2aa
logickoff123 <SonyaUstinova@gmail.com>
2024-11-19 20:03:51 +0300

    tets+2 blocks

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import styles from './Test.module.css'
 
 const Test = () => {
   return (
-    <div>
-      <h1>Добро пожаловать на новую страницу!</h1>
+    <div className={styles.block}> 
+      <h1 className = {styles.text1}>Создание тестa</h1>
+        <div className ={styles.block1}>
+          <div className = {styles.block2}>
+
+          </div>
+      </div>
     </div>
   );
 };
