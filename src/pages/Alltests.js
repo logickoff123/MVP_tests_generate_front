a54045b7a327d9a3568f45a9dfe3b743480ad9e1
logickoff123 <SonyaUstinova@gmail.com>
2024-11-26 16:52:05 +0300

    mathtest (waring with flex)

--- src/pages/Alltests.js
+++ src/pages/Alltests.js
@@ -4,16 +4,18 @@ import styles from "./Alltests.module.css" ;
 const Alltests = () => {
   return (
     <div className = {styles.main_contanier}>
-      <div classname = {styles.nomer_zadaniya}>
-        <p className= {styles.text1}> Задание №1 </p>
-        <div className  = {styles.task}>
-          <p className= {styles.text2}>Задана функция y = 5x-8.Найдите ее значения при x = 2 </p>
-          <div className = {styles.otvet}>
-            <button className = {styles.blue}>20</button>
-            <button className = {styles.green}>20</button>
-            <button className = {styles.orange}>20</button>
-            <button className = {styles.pink}>20</button>
+      <div className = {styles.all_contanier}>
+        <div className = {styles.nomer_zadaniya}>
+          <p className= {styles.text1}> Задание №1 </p>
+          <div className  = {styles.task}>
+            <p className= {styles.text2}>Задана функция y = 5x-8.Найдите ее значения при x = 2 </p>
           </div>
+            <div className = {styles.otvet}>
+              <button className = {styles.blue}>20</button>
+              <button className = {styles.green}>20</button>
+              <button className = {styles.orange}>20</button>
+              <button className = {styles.pink}>20</button>
+            </div>
         </div>
       </div>
     </div>
