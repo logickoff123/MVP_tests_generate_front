2cce4628e3f85c18d01a84a1196ce2d31ba5922d
logickoff123 <SonyaUstinova@gmail.com>
2024-11-26 11:34:08 +0300

    pravki_allpages

--- src/pages/Alltests.js
+++ src/pages/Alltests.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styles from "./Alltests.module.css" ;
 
 const Alltests = () => {
   return (
-    <div>
-      <h1>Добро пожаловать на новую страницу!</h1>
+    <div className = {styles.main_contanier}>
+      <div className = {styles.task1}>
+        <div className = {styles.text_1}>
+          <p className={styles.t_1}>Задана функция y = 5x-8.Найдите ее значения при x=2 </p>
+            <div className = {styles.answer_1}>
+              <p className = {styles.a_1}>20</p>
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
