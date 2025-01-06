5d271bdbdd65e9e6a8e5ba1a824f9f185985f6ce
nikelborm <evadev@duck.com>
2025-01-06 15:45:22 +0300

    quick save: Mon Jan  6 15:45:22 MSK 2025

--- src/pages/Alltests.js
+++ src/pages/Alltests.js
@@ -3,12 +3,11 @@ import styles from "./Alltests.module.css" ;
 
 const Alltests = () => {
   return (
-    <div className = {styles.main_contanier}>
-      <div className = {styles.all_contanier}>
+    <div className = {styles.main_container}>
         <div className = {styles.nomer_zadaniya}>
           <p className= {styles.text1}> Задание №1 </p>
           <div className  = {styles.task}>
-            <p className= {styles.text2}>Задана функция y = 5x-8.Найдите ее значения при x = 2 </p>
+            <p className= {styles.text2}>Задана функция функция функция функция функция функция функция функция y = 5x-8.Найдите ее значения при x = 2 </p>
           </div>
             <div className = {styles.otvet}>
               <button className = {styles.blue}>20</button>
@@ -17,7 +16,6 @@ const Alltests = () => {
               <button className = {styles.pink}>20</button>
             </div>
         </div>
-      </div>
     </div>
   );
 };
