02f45ca300f57a2fe291742740eb752672d026b8
nikelborm <evadev@duck.com>
2025-01-06 16:03:54 +0300

    quick save: Mon Jan  6 16:03:54 MSK 2025

--- src/pages/Alltests.js
+++ src/pages/Alltests.js
@@ -2,22 +2,102 @@ import React from 'react';
 import styles from "./Alltests.module.css" ;
 
 const Alltests = () => {
+  const questions = [{
+    questionIndex: 1,
+    questionText: 'Задана функция функция функция функция функция функция функция функция y = 5x-8.Найдите ее значения при x = 2',
+    answers: [
+      { className: styles.blue, text: '10' },
+      { className: styles.green, text: '12' },
+      { className: styles.orange, text: '20' },
+      { className: styles.pink, text: '56' },
+    ]
+  }, {
+    questionIndex: 2,
+    questionText: '2+2?',
+    answers: [
+      { className: styles.blue, text: '5' },
+      { className: styles.green, text: '4' },
+      { className: styles.orange, text: 'NaN' },
+      { className: styles.pink, text: 'RRRRRRRRRR' },
+    ]
+  }, {
+    questionIndex: 4,
+    questionText: '2+2?',
+    answers: [
+      { className: styles.blue, text: '5' },
+      { className: styles.green, text: '4' },
+      { className: styles.orange, text: 'NaN' },
+      { className: styles.pink, text: 'RRRRRRRRRR' },
+    ]
+  }, {
+    questionIndex: 5,
+    questionText: '2+2?',
+    answers: [
+      { className: styles.blue, text: '5' },
+      { className: styles.green, text: '4' },
+      { className: styles.orange, text: 'NaN' },
+      { className: styles.pink, text: 'RRRRRRRRRR' },
+    ]
+  }];
+
   return (
     <div className = {styles.main_container}>
-        <div className = {styles.nomer_zadaniya}>
-          <p className= {styles.text1}> Задание №1 </p>
-          <div className  = {styles.task}>
-            <p className= {styles.text2}>Задана функция функция функция функция функция функция функция функция y = 5x-8.Найдите ее значения при x = 2 </p>
-          </div>
-            <div className = {styles.otvet}>
-              <button className = {styles.blue}>20</button>
-              <button className = {styles.green}>20</button>
-              <button className = {styles.orange}>20</button>
-              <button className = {styles.pink}>20</button>
-            </div>
-        </div>
+      <ManyQuestionsComponent questions={questions}/>
     </div>
   );
 };
 
 export default Alltests;
+
+
+
+/**
+ * Description placeholder
+ *
+ * @param {{ questions: { questionIndex: number; questionText: string; answers: { text: string, className: string }[]; }[] }} param0 
+ * @param {{ questionIndex: number; questionText: string; answers: { text: string, className: string }[]; }[]} param0.questions 
+ * @returns {*} 
+ */
+const ManyQuestionsComponent = ({ questions }) => <>
+  {
+    questions.map((props) => (
+      <QuestionComponent key={props.questionIndex} {...props}/>
+    ))
+  }
+</>
+
+const questions = [{
+  questionIndex: 1,
+  questionText: 'Задана функция функция функция функция функция функция функция функция y = 5x-8.Найдите ее значения при x = 2',
+  answers: [
+    { className: styles.blue, text: '10' },
+    { className: styles.green, text: '12' },
+    { className: styles.orange, text: '20' },
+    { className: styles.pink, text: '56' },
+  ]
+}]
+
+
+
+/**
+ * Description placeholder
+ *
+ * @param {{ questionIndex: number; questionText: string; answers: { text: string, className: string }[]; }} param0
+ * @param { number } param0.questionIndex
+ * @param { string } param0.questionText
+ * @param { { text: string, className: string }[] } param0.answers
+ * @returns {*}
+ */
+const QuestionComponent = ({ questionIndex, questionText, answers }) => (
+  <div className = {styles.nomer_zadaniya}>
+    <p className= {styles.text1}> Задание №{questionIndex} </p>
+    <div className  = {styles.task}>
+      <p className= {styles.text2}>{questionText}</p>
+    </div>
+    <div className = {styles.otvet}>
+      {answers.map(({text, className}, i) => (
+        <button key={i} className = {className}>{text}</button>
+      ))}
+    </div>
+  </div>
+)
