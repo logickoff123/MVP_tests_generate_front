f5dbd22b76231cca65c0093fb097c93ee05f7e75
Andrew Pavin <2003and@gmail.com>
2025-01-08 00:52:25 +0300

    Minor tweaks

---
+++ src/hooks/hooks.js
@@ -0,0 +1,44 @@
+import { useQuery } from '@tanstack/react-query';
+
+export const useProductsQuery = () => {
+    return useQuery({
+        queryKey: ['generate_question'],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:8000/generate_question`);
+            return await response.json();
+        }
+    });
+}
+
+/*
+export const useProductsQuery = () => {
+    return useQuery({
+        queryKey: ['all_product'],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:3001/api/product/all`);
+            return await response.json();
+        }
+    });
+}
+export const useSpecificProductQuery = (productId) => {
+    return useQuery({
+        queryKey: ['specific_product', productId],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:3001/api/product/${productId}`);
+            return await response.json();
+        }
+    });
+}
+
+export const useCategoriesQuery = () => {
+    return useQuery({
+        queryKey: ['all_category'],
+        /** @returns {Promise<{ asd: 'asd' }>} */
+/*
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:3001/api/category/all`);
+            return await response.json();
+        }
+    });
+}
+*/
