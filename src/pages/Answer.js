e627b1e3ed090a9c3bf12ff3e00cf94ea18a60a9
Andrew Pavin <2003and@gmail.com>
2025-01-15 21:32:22 +0300

    usage of useAllAnswersQuery

--- src/pages/Answer.js
+++ src/pages/Answer.js
@@ -5,8 +5,14 @@ import {useAllAnswersQuery} from "../hooks/hooks";
 
 
 const Answer = () => {
-    const {total_answers, correct_answers} = useAllAnswersQuery()
-
+    // const {total_answers, correct_answers} = useAllAnswersQuery()
+    var correct_answers
+    var total_answers
+    var data_raw = useAllAnswersQuery();
+    if (data_raw["isSuccess"]) {
+      total_answers = data_raw.data["total_answers"]
+      correct_answers = data_raw.data["correct_answers"]
+    }
     const navigate = useNavigate();
     const handleMain = () => {
       navigate('/');
