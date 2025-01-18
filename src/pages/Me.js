3a36141badbf0168ddd90833d8020578bccc561b
nikelborm <evadev@duck.com>
2025-01-19 00:18:36 +0300

    quick save: Sun Jan 19 00:18:36 MSK 2025

--- src/pages/Me.js
+++ src/pages/Me.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useUserMetadata } from '../hooks/useUserMetadata';
 import { useMeUserQuery } from '../hooks/hooks';
+import { useAuth0 } from '@auth0/auth0-react';
 
 
 function Me() {
-  const { data: UserMetadataFromAuth0 } = useUserMetadata();
+  const { user: UserMetadataFromAuth0Hook } = useAuth0();
+  const { data: UserMetadataFromAuth0ManualRequest } = useUserMetadata();
   const { data: UserMetadataFromServer } = useMeUserQuery();
 
   return <pre style={{ color: 'white' }}>{JSON.stringify({
-    UserMetadataFromAuth0,
+    UserMetadataFromAuth0Hook,
+    UserMetadataFromAuth0ManualRequest,
     UserMetadataFromServer
   }, null, 4)}</pre>;
 }
